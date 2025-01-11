152103e17ea5cda0543f366a4b98e1c9911a2715
Михаил Пилипенко <Mihail.pilipenko2002@mail.ru>
2025-01-11 16:21:18 +0300

    modified for work protection

--- PARALEL_RGR/PARALEL_RGR.cpp
+++ PARALEL_RGR/PARALEL_RGR.cpp
@@ -83,12 +83,12 @@ void fillRandom(std::vector<std::unique_ptr<Base>>& vec, int n, int max = 10000)
         int t = rand() % max;
         if (t % 2 == 0)
         {
-            vec.push_back(std::make_unique<IntElement>(t));
+            vec.push_back(std::make_unique<IntElement>((rand()%2<0.5)?t:-t));
         }
         else
         {
-            double t2 = rand() % max / 10000.0;
-            vec.push_back(std::make_unique<DoubleElement>(t + t2));
+            double t2 = rand() % max / (double)max;
+            vec.push_back(std::make_unique<DoubleElement>((rand() % 2 < 0.5) ? t + t2 : -(t + t2)));
         }
        
     }
@@ -148,23 +148,81 @@ std::vector<int> multiThreadSearch(std::vector<std::unique_ptr<Base>>& vec, T va
 int main() {
     srand(time(0));
     std::vector<std::unique_ptr<Base>> elements;
-    int testNum = 50;
+    std::setlocale(LC_ALL, "rus");
+    fillRandom(elements, 200, 50);
+    for (const auto& e : elements)
+    {
+        e->print();
+        std::cout << " ";
+    }
+    std::cout << std::endl;
+    /*int choose = 0;
+    while(choose != 1 && choose != 2)
+    {
+        std::cout << "Выберите, какое число вы хотите искать:\n1-Целое\n2-Вещественное" << std::endl;
+        std::cin >> choose;
+    }*/
+    double precision = 0.00000;
+    int testNum = 1;
+    double userInpDouble = 0.0000;
+    int userInpInt = 0;
+    double userInp = 0.0;
+    std::cout << "Введите точность, с которой хотите искать число:" << std::endl;
+    std::cin >> precision;
+    std::cout << "Введите число, которое хотите искать, с точностью " << precision << std::endl;
+    std::cin >> userInp;
+    std::vector<int> tmp = multiThreadSearch(elements, userInp, 1, precision);
+    for (auto el : tmp) {
+        elements[el]->print();
+        std::cout << " ";
+    }
+    std::cout << std::endl;
+    /*if (choose == 1)
+    {
+        std::cout << "Введите целое число, которое хотите искать, с точностью " << precision << std::endl;
+        std::cin >> userInpInt;
+    }
+    else {
+        std::cout << "Введите вещественное число, которое хотите искать, с точностью " << precision << std::endl;
+        std::cin >> userInpDouble;
+    }*/
     for (int threadCount = 1; threadCount < 17; threadCount++)
     {
         std::cout << "-------------------------------------------------------------------\nTests for " << threadCount << " threads in search"<<std::endl;
         for (int i = 3; i < 8; i++)
         {
             //std::cout << "Vector length: " << pow(10, i) << std::endl;
-            fillRandom(elements, pow(10, i), 10000);
             double averageTime = 0.0;
             for (int j = 0; j < testNum; j++)
             {
+                fillRandom(elements, pow(10, i), 10000);
                 const auto start = std::chrono::high_resolution_clock::now();
-                std::vector<int> res = multiThreadSearch(elements, 500, threadCount, 0.001);
+                std::vector<int> res;
+                /*if (choose == 1)
+                {
+                    res = multiThreadSearch(elements, userInpInt, threadCount, precision);
+                    const auto end = std::chrono::high_resolution_clock::now();
+                    const std::chrono::duration<double, std::milli> elapsed = end - start;
+                    std::cout << "Attempt " << j + 1 << " | found " << res.size() << " elements equal " << userInpInt << " with precision "<<precision<<" in  " << elapsed.count() << " ms" << std::endl;
+                    averageTime += elapsed.count();
+                }
+                else {
+                    res = multiThreadSearch(elements, userInpDouble, threadCount, precision);
+                    const auto end = std::chrono::high_resolution_clock::now();
+                    const std::chrono::duration<double, std::milli> elapsed = end - start;
+                    std::cout << "Attempt " << j + 1 << " | found " << res.size() << " elements equal " << userInpDouble << " with precision " << precision << " in  " << elapsed.count() << " ms" << std::endl;
+                    averageTime += elapsed.count();
+                }*/
+                res = multiThreadSearch(elements, userInp, threadCount, precision);
                 const auto end = std::chrono::high_resolution_clock::now();
                 const std::chrono::duration<double, std::milli> elapsed = end - start;
-                //std::cout << "Attempt " << j + 1 << " | found " << res.size() << " elements in  " << elapsed.count() << " ms" << std::endl;
+                std::cout << "Attempt " << j + 1 << " | found " << res.size() << " elements equal " << userInp << " with precision " << precision << " in  " << elapsed.count() << " ms" << std::endl;
                 averageTime += elapsed.count();
+                for (auto el : res) {
+                    elements[el]->print();
+                    std::cout << " ";
+                }
+                std::cout << std::endl;
             }
             std::cout << "Average time for " << pow(10, i) << " elements in vector: " << averageTime / testNum << " ms" << std::endl;
         }
